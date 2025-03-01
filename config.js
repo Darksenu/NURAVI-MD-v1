ae248a88d53236183b863c0979835d2c0d1984d9
cKT CHANU <notychanu79@gmail.com>
2025-03-02 01:20:00 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+module.exports = {
+AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
+STATUS_READ_MSG: process.env.STATUS_READ_MSG || "Didula MD V2 💚",
+ANTI_DELETE: process.env.ANTI_DELETE || "true",
+ANTI_CALL: process.env.ANTI_CALL || "true",
+FOOTER: process.env.FOOTER || "*◆─〈 ✦𝐃𝐢𝐝𝐮𝐥𝐚 𝐌𝐃 𝐕𝟐✦ 〉─◆*",
+AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "true",
+IMAGE_LIMIT: process.env.IMAGE_LIMIT || "3",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/tC37Q7B/20241220-122443.jpg",
+SESSION_ID: process.env.SESSION_ID || "",
+};
+
+
